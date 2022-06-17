0923039f0c67e303046c3587a310c2f5f13823b6
DET171 <mrgeek484@gmail.com>
2022-06-17 18:40:32 +0800

    e

--- firebase.creds.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-	apiKey: 'AIzaSyAcd3tUTTQ0bHKznyC2OzQNw0gZKvql8lA',
-	authDomain: 'vue-todos-app-f286d.firebaseapp.com',
-	projectId: 'vue-todos-app-f286d',
-	storageBucket: 'vue-todos-app-f286d.appspot.com',
-	messagingSenderId: '903444734691',
-	appId: '1:903444734691:web:521ee236cda1012b8a9bd6',
-	measurementId: 'G-N8D32YZ4XD',
-};
